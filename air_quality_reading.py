052975c2ee4559efb826f2b9a0c8f8975d8d4647
Andrew Hao <andrewhao@gmail.com>
2020-09-17 06:54:57 -0700

    Adding air quality reading

---
+++ air_quality_reading.py
@@ -0,0 +1,21 @@
+import requests
+from pms7003 import Pms7003Sensor, PmsSensorException
+
+class AirQualityReading:
+    def __init__(self):
+        self.sensor = Pms7003Sensor('/dev/serial0');
+        self.data = None
+
+    def run(self):
+        self.data = sensor.read()
+        self.report(data)
+        self.sensor.close()
+
+    def report(self, data):
+        data['device_id'] = os.environ['HOSTNAME']
+        response = requests.post('http://thermonoto.herokuapp.com/air_quality_updates', data=data)
+        print('Posting with', data)
+        print(response)
+
+if __name__ == '__main__':
+    AirQualityReading().run()
